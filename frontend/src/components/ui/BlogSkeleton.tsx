c7f0cb29163ee5de9677bb85de7f39c595c66538
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-03-04 22:05:48 +0530

    Added skeleton

---
+++ frontend/src/components/ui/BlogSkeleton.tsx
@@ -0,0 +1,22 @@
+
+const BlogSkeleton = () => {
+  return (
+    <div className="px-24">
+        <div role="status" className="animate-pulse pt-8" >
+            <div className="h-16 bg-gray-200 rounded-full w-full mb-4"></div>
+            <div className="h-1 bg-gray-200 rounded-full max-w-[200px] mb-2.5"></div>
+
+            <div className="pt-4">
+                <div className="h-2 bg-gray-200 rounded-full max-w-[360px] mb-2.5"></div>
+                <div className="h-2 bg-gray-200 rounded-full mb-2.5"></div>
+                <div className="h-2 bg-gray-200 rounded-full max-w-[330px] mb-2.5"></div>
+                <div className="h-2 bg-gray-200 rounded-full max-w-[800px] mb-2.5"></div>
+                <div className="h-2 bg-gray-200 rounded-full max-w-[360px]"></div>
+                <span className="sr-only">Loading...</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BlogSkeleton
