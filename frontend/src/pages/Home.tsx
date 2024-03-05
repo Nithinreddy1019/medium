24e2bf4f7ca6c09fb0ed968967030d8a1baeb8b0
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-03-05 22:49:46 +0530

    Updated components

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { BACKEND_URL } from "../config";
+
+const Home = () => {
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/`, {
+            headers: {
+                "Authorization": "Bearer "+ localStorage.getItem("token")
+            }
+        }).then((res) => {
+            if(res.data.message === "Unauthorized"){
+                navigate("/singup")
+            } else {
+                navigate("/blogs")    
+            }
+            
+        })
+    }, [])
+
+    
+
+  return (
+    <div>
+      ffff
+    </div>
+  )
+}
+
+export default Home
